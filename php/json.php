690d51b330e99f9ff1bfa7e05ad6cd66dc76992a
Adalmaer <mail@maximiliankrieg.de>
2016-07-11 11:38:54 +0200

    Cord

--- php/json.php
+++ php/json.php
@@ -71,6 +71,8 @@ class _Jump extends Page
      */
     protected function getViewData()
     {
+		error_reporting(-1);
+		
         // to do: fetch data for this view from the 
 		$queryList = array();
 		/*$queryList[0][0] ="SELECT system_id AS systemId, system_name as systemName, faction_id as factionId, system_x as systemX, system_y as systemY, system_z as systemZ, quadrant_id as quadrantId, system_status as systemStatus FROM wcrs_system ORDER BY sector_id ASC, quadrant_id ASC, system_id ASC";
@@ -86,7 +88,7 @@ class _Jump extends Page
 		$queryList[5][0] ="SELECT fleet_id, fleet_name, faction_id, fleet_image, system_id, fleet_status FROM wcrs_fleet ORDER BY fleet_id ASC";
 		$queryList[5][1] = "fleet";*/
 		
-		$queryList[0][0] = "SELECT system_id AS systemId, system_name as systemName, system_x as systemX, system_y as systemY, system_z AS systemZ, faction_id AS factionId, q.quadrant_id as quadrantId, quadrant_name AS quadrantName, quadrant_x as quadrantX, quadrant_y AS quadrantY, r.sector_id AS sectorId, r.sector_name AS sectorName, system_status AS systemStatus FROM wcrs_system s, wcrs_quadrant q, wcrs_sector r WHERE s.quadrant_id=q.quadrant_id AND q.sector_id = r.sector_id ORDER BY r.sector_id ASC, s.quadrant_id ASC, s.system_id ASC;";
+		$queryList[0][0] = "SELECT system_id AS systemId, system_name as systemName, system_x as systemX, system_y as systemY, system_z AS systemZ, faction_id AS factionId, q.quadrant_id as quadrantId, quadrant_name AS quadrantName, quadrant_x as quadrantX, quadrant_y AS quadrantY, r.sector_id AS sectorId, r.sector_name AS sectorName, system_status AS systemStatus FROM wcrs_system s, wcrs_quadrant q, wcrs_sector r WHERE s.quadrant_id=q.quadrant_id AND q.sector_id = r.sector_id AND system_status >0 ORDER BY r.sector_id ASC, s.quadrant_id ASC, s.system_id ASC;";
 		$queryList[0][1] = "system";
 		
 		$queryList[1][0] = "SELECT fleet_id AS fleetId, fleet_name AS fleetName, fleet_image as fleetImage, system_x AS systemX, system_y AS systemY, quadrant_x AS quadrantX, quadrant_y AS quadrantY, color_code AS colorCode FROM wcrs_system s, wcrs_quadrant q, wcrs_fleet f, wcrs_faction n WHERE n.faction_id = f.faction_id AND s.quadrant_id=q.quadrant_id AND s.system_id = f.system_id AND fleet_status >0 ORDER BY fleet_id ASC;";
@@ -95,6 +97,11 @@ class _Jump extends Page
 		$queryList[2][0] ="SELECT faction_id AS factionId, faction_name AS factionName, color_code AS colorCode FROM wcrs_faction ORDER BY faction_id ASC";
 		$queryList[2][1] = "faction";
 		
+		$queryList[3][0] ="SELECT q.quadrant_x AS quadrantX, q.quadrant_y AS quadrantY, q.quadrant_name AS quadrantName, r.sector_id AS sectorId, r.sector_name AS sectorName FROM wcrs_quadrant q, wcrs_sector r WHERE q.sector_id=r.sector_id ORDER BY r.sector_id ASC, quadrant_id ASC";
+		$queryList[3][1] = "quadrant";
+		
+		$queryList[4][0] = "SELECT (((q1.quadrant_x - 1)*30)+s1.system_x)*50 AS jumpX1, (((q1.quadrant_y - 1)*30)+s1.system_Y)*50 AS jumpZ1,(((q2.quadrant_x - 1)*30)+s2.system_x)*50 AS jumpX2,(((q2.quadrant_y - 1)*30)+s2.system_y)*50 AS jumpZ2 FROM wcrs_system s1, wcrs_jump j, wcrs_system s2,wcrs_quadrant q1,wcrs_quadrant q2 WHERE s1.system_id = j.system_id_1 AND s1.quadrant_id = q1.quadrant_id AND  s2.system_id = j.system_id_2 AND s2.quadrant_id = q2.quadrant_id;";
+		$queryList[4][1] = "jump";
 		
 		$json = '';
 		
@@ -137,13 +144,7 @@ class _Jump extends Page
 
 		} else {
 			updateNotification($filename, 'Die Datei ist nicht schreibbar',1);
-	}
-		
-		
-		
-		
-
-		
+		}
     }
     
     /**
